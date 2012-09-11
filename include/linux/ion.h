dd4b7ff0af7fb1a13eb3eb2e3a2db531b4c2da55
Mitchel Humpherys <mitchelh@codeaurora.org>
2012-09-11 15:59:11 -0700

    ion: change ion buffer flags
    This syncs up our ion buffer flags in ion.h with upstream (using
    ION_FLAG_CACHED instead of CACHED and UNCACHED).
    
    Change-Id: I2e07a3acba230a86817182677fa2394fa846fd7e
    Signed-off-by: Mitchel Humpherys <mitchelh@codeaurora.org>

--- include/linux/ion.h
+++ include/linux/ion.h
@@ -37,7 +37,7 @@ enum ion_heap_type {
 #define ION_HEAP_CARVEOUT_MASK		(1 << ION_HEAP_TYPE_CARVEOUT)
 #define ION_HEAP_CP_MASK		(1 << ION_HEAP_TYPE_CP)
 
-
+#define ION_FLAG_CACHED 1
 
 enum ion_heap_ids {
 	INVALID_HEAP_ID = -1,
@@ -92,11 +92,10 @@ enum cp_mem_usage {
 #define CACHED          1
 #define UNCACHED        0
 
-#define ION_CACHE_SHIFT 0
+#define ION_SET_CACHED(__cache)		(__cache | ION_FLAG_CACHED)
+#define ION_SET_UNCACHED(__cache)	(__cache & ~ION_FLAG_CACHED)
 
-#define ION_SET_CACHE(__cache)  ((__cache) << ION_CACHE_SHIFT)
-
-#define ION_IS_CACHED(__flags)	((__flags) & (1 << ION_CACHE_SHIFT))
+#define ION_IS_CACHED(__flags)	((__flags) & ION_FLAG_CACHED)
 
 #define ION_IOMMU_UNMAP_DELAYED 1
 
