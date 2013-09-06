43807437599c3664bc1c25d44b04b87f4110fbe4
bilalliberty <bilal_liberty@live.com>
2013-09-07 01:51:08 +0300

    repair s2w and delete d2w for now

--- include/linux/input/cy8c_cs.h
+++ include/linux/input/cy8c_cs.h
@@ -4,71 +4,71 @@
 
 #include <linux/types.h>
 
-#define CYPRESS_CS_NAME "CY8C20224"
-#define CYPRESS_SS_NAME "CY8C21x34B"
+#define CYPRESS_CS_NAME 	"CY8C20224"
+#define CYPRESS_SS_NAME 	"CY8C21x34B"
 
 #if defined(CONFIG_TOUCH_KEY_FILTER)
 #include <linux/notifier.h>
 #endif
 
-#define CS_STATUS (uint8_t) 0x00
+#define CS_STATUS		(uint8_t) 0x00
 
-#define CS_FW_VERSION (uint8_t) 0x01
-#define CS_FW_CONFIG (uint8_t) 0xAA
+#define CS_FW_VERSION		(uint8_t) 0x01
+#define CS_FW_CONFIG		(uint8_t) 0xAA		
 
-#define CS_IDAC_BTN_BASE (uint8_t) 0x02
-#define CS_IDAC_BTN_PAD1 (uint8_t) 0x02
-#define CS_IDAC_BTN_PAD2 (uint8_t) 0x03
-#define CS_IDAC_BTN_PAD3 (uint8_t) 0x04
-#define CS_IDAC_BTN_PAD4 (uint8_t) 0x05
+#define CS_IDAC_BTN_BASE	(uint8_t) 0x02
+#define CS_IDAC_BTN_PAD1	(uint8_t) 0x02
+#define CS_IDAC_BTN_PAD2        (uint8_t) 0x03
+#define CS_IDAC_BTN_PAD3        (uint8_t) 0x04
+#define CS_IDAC_BTN_PAD4        (uint8_t) 0x05
 
-#define CS_MODE (uint8_t) 0x06
-#define CS_DTIME (uint8_t) 0x07
-#define CS_SLEEPTIME (uint8_t) 0x08
-#define CS_FW_CHIPID (uint8_t) 0x0A
-#define CS_FW_KEYCFG (uint8_t) 0x0B
+#define CS_MODE			(uint8_t) 0x06
+#define CS_DTIME		(uint8_t) 0x07
+#define CS_SLEEPTIME		(uint8_t) 0x08
+#define CS_FW_CHIPID		(uint8_t) 0x0A		
+#define CS_FW_KEYCFG		(uint8_t) 0x0B		
 
-#define CS_SELECT (uint8_t) 0x0C
-#define CS_BL_HB (uint8_t) 0x0D
-#define CS_BL_LB (uint8_t) 0x0E
-#define CS_RC_HB (uint8_t) 0x0F
-#define CS_RC_LB (uint8_t) 0x10
-#define CS_DF_HB (uint8_t) 0x11
-#define CS_DF_LB (uint8_t) 0x12
-#define CS_INT_STATUS (uint8_t) 0x13
+#define CS_SELECT		(uint8_t) 0x0C
+#define CS_BL_HB		(uint8_t) 0x0D
+#define CS_BL_LB		(uint8_t) 0x0E
+#define CS_RC_HB		(uint8_t) 0x0F
+#define	CS_RC_LB		(uint8_t) 0x10
+#define CS_DF_HB		(uint8_t) 0x11
+#define CS_DF_LB		(uint8_t) 0x12
+#define CS_INT_STATUS		(uint8_t) 0x13
 
 
-#define CS_CMD_BASELINE (0x55)
-#define CS_CMD_DSLEEP (0x02)
-#define CS_CMD_BTN1 (0xA0)
-#define CS_CMD_BTN2 (0xA1)
-#define CS_CMD_BTN3 (0xA2)
-#define CS_CMD_BTN4 (0xA3)
+#define CS_CMD_BASELINE		(0x55)
+#define CS_CMD_DSLEEP		(0x02)
+#define CS_CMD_BTN1		(0xA0)
+#define CS_CMD_BTN2		(0xA1)
+#define CS_CMD_BTN3		(0xA2)
+#define CS_CMD_BTN4		(0xA3)
 
-#define CS_CHIPID (0x36)
-#define CS_KEY_4 (0x04)
-#define CS_KEY_3 (0x03)
+#define CS_CHIPID		(0x36)
+#define CS_KEY_4		(0x04)
+#define CS_KEY_3		(0x03)
 
-#define CS_FUNC_PRINTRAW (0x01)
+#define CS_FUNC_PRINTRAW	(0x01)
 
-#define ENABLE_CAP_ONLY_3KEY 1
+#define ENABLE_CAP_ONLY_3KEY   1        
 
 struct infor {
-uint8_t config;
-uint16_t chipid;
-uint16_t version;
+	uint8_t  config;
+	uint16_t chipid;
+	uint16_t version;
 };
 
 struct cy8c_i2c_cs_platform_data {
-struct infor id;
-uint16_t gpio_rst;
-uint16_t gpio_irq;
-int (*power)(int on);
-int (*reset)(void);
-int	keycode[4];
-void (*gpio_init)(void);
-int func_support;
-int prj_info;
+	struct 	infor id;
+	uint16_t gpio_rst;
+	uint16_t gpio_irq;
+	int 	(*power)(int on);
+	int 	(*reset)(void);
+	int	keycode[4];
+	void 	(*gpio_init)(void);
+	int 	func_support;
+	int     prj_info;
 };
 
 
@@ -79,12 +79,12 @@ extern int register_notifier_by_touchkey(struct notifier_block *nb);
 extern int unregister_notifier_by_touchkey(struct notifier_block *nb);
 #endif
 
+extern uint8_t touchscreen_is_on(void); 
+
 #ifdef CONFIG_TOUCHSCREEN_CYPRESS_SWEEP2WAKE
 #include <linux/input.h>
 /* Sweep2Wake */
 extern void sweep2wake_setdev(struct input_dev * input_device);
 #endif
-
-extern uint8_t touchscreen_is_on(void);
 
 #endif
