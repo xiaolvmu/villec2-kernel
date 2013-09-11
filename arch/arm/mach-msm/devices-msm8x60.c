4c141285f9d4e84c74e35fb24d0bda1e0257b744
bilalliberty <bilal_liberty@live.com>
2013-09-11 06:38:18 +0300

    OC from Sebestianfm's kernel

--- arch/arm/mach-msm/devices-msm8x60.c
+++ arch/arm/mach-msm/devices-msm8x60.c
@@ -887,6 +887,16 @@ static struct resource kgsl_3d0_resources[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
+			.gpu_freq = 320000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
+			.gpu_freq = 300000000,
+			.bus_freq = 4,
+			.io_fraction = 0,
+		},
+		{
 			.gpu_freq = 266667000,
 			.bus_freq = 4,
 			.io_fraction = 0,
@@ -911,8 +921,8 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 0,
 		},
 	},
-	.init_level = 0,
-	.num_levels = 5,
+	.init_level = 2,
+	.num_levels = 7,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/5,
 	.nap_allowed = true,
