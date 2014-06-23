d84671bd82c4d3cf95b6b8827cb224b0cc54db02
Sultanxda <sultanxda@gmail.com>
2014-06-23 11:52:42 -0700

    video: msm: Allow timeouts for DSI operations
    Based off of https://github.com/CyanogenMod/android_kernel_samsung_msm8660-common/commit/996fdd8d496e1c8bc6eea4abd38a066cc8d3304a
    
    Signed-off-by: Sultanxda <sultanxda@gmail.com>

--- drivers/video/msm/mdp4_overlay_dsi_cmd.c
+++ drivers/video/msm/mdp4_overlay_dsi_cmd.c
@@ -505,7 +505,8 @@ static void mdp4_dsi_cmd_wait4dmap(int cndx)
 	if (atomic_read(&vctrl->suspend) > 0)
 		return;
 
-	wait_for_completion(&vctrl->dmap_comp);
+	if (!wait_for_completion_timeout(&vctrl->dmap_comp, msecs_to_jiffies(100)))
+		pr_err("%s %d  TIMEOUT_\n", __func__, __LINE__);
 }
 
 static void mdp4_dsi_cmd_wait4ov(int cndx)
@@ -522,7 +523,8 @@ static void mdp4_dsi_cmd_wait4ov(int cndx)
 	if (atomic_read(&vctrl->suspend) > 0)
 		return;
 
-	wait_for_completion(&vctrl->ov_comp);
+	if (!wait_for_completion_timeout(&vctrl->ov_comp, msecs_to_jiffies(100)))
+		pr_err("%s %d  TIMEOUT_\n", __func__, __LINE__);
 }
 
 /*
