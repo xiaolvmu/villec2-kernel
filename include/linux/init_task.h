3b76cb0ae0d3d1e66ec937b3d59581857f840986
xiaolvmu <i@xiaolvmu.me>
2015-06-16 23:41:57 +0800

    Cause errors

--- include/linux/init_task.h
+++ include/linux/init_task.h
@@ -123,15 +123,6 @@ extern struct group_info init_groups;
 
 extern struct cred init_cred;
 
-extern struct task_group root_task_group;
-
-#ifdef CONFIG_CGROUP_SCHED
-# define INIT_CGROUP_SCHED(tsk)						\
-	.sched_task_group = &root_task_group,
-#else
-# define INIT_CGROUP_SCHED(tsk)
-#endif
-
 #ifdef CONFIG_PERF_EVENTS
 # define INIT_PERF_EVENTS(tsk)						\
 	.perf_event_mutex = 						\
