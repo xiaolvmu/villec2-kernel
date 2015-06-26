0726f16f68a310ce3a5ebdec8d3eea648b810895
xiaolvmu <i@xiaolvmu.me>
2015-06-26 19:01:50 +0800

    Use badass by default now

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -336,9 +336,6 @@ extern struct cpufreq_governor cpufreq_gov_intellidemand;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_smartass2)
-#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIMM)
-extern struct cpufreq_governor cpufreq_gov_intellimm;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intellimm)
 #endif
 
 
